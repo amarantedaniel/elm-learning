5d9d036c4d2c5829b80b7789edc0ce67fb4cfd62
Daniel Amarante <daniel.amarante2@gmail.com>
2018-01-24 14:22:01 -0200

    [#00] Add good part of the todos implementation

--- app/elm/app/Main.elm
+++ app/elm/app/Main.elm
@@ -2,71 +2,144 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onMouseOver, onClick)
+import Html.Events exposing (on, keyCode, onInput)
+import Json.Decode as Json
 
 
--- MODEL
+type FilterState
+    = All
+    | Active
+    | Completed
 
 
-type alias Model =
-    { rightButton : String
-    , leftButton : String
-    , response : String
+type alias Todo =
+    { title : String
+    , completed : Bool
+    , editing : Bool
     }
 
 
-model : Model
-model =
-    Model "YES" "NO" ""
+type alias Model =
+    { todos : List Todo
+    , todo : Todo
+    , filter : FilterState
+    }
+
+
+initialModel : Model
+initialModel =
+    { todos = []
+    , todo =
+        { title = ""
+        , completed = False
+        , editing = False
+        }
+    , filter = All
+    }
+
+
+newTodo : Todo
+newTodo =
+    { title = ""
+    , completed = False
+    , editing = False
+    }
 
 
 
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ h1 [ style [ ( "color", "#CB8F36" ) ] ] [ text "IS FERNANDO HECK GAY?" ]
-        , div [ class "daniwrapper" ]
-            [ div [ class "danibutton" ] [ button [ class "danibutton", onMouseOver Left, onClick (Click model.leftButton) ] [ text model.leftButton ] ]
-            , div [ class "danibutton" ] [ button [ class "danibutton", onMouseOver Right, onClick (Click model.rightButton) ] [ text model.rightButton ] ]
-            ]
-        , h1 [ class "danitext" ] [ text model.response ]
-        ]
-
-
-
--- UPDATE
+-- Update
 
 
 type Msg
-    = Left
-    | Right
-    | Click String
+    = UpdateTitle String
+    | Add
+    | Complete Todo
+    | Delete Todo
+    | Filter FilterState
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Left ->
-            { model | leftButton = "YES", rightButton = "NO" }
+        UpdateTitle str ->
+            let
+                todo =
+                    model.todo
 
-        Right ->
-            { model | leftButton = "NO", rightButton = "YES" }
+                updatedTodo =
+                    { todo | title = str }
+            in
+                { model | todo = updatedTodo }
 
-        Click text ->
-            { model | response = text }
+        Add ->
+            { model
+                | todos = model.todo :: model.todos
+                , todo = newTodo
+            }
+
+        Complete todo ->
+            model
+
+        Delete todo ->
+            model
+
+        Filter filterState ->
+            model
+
+
+onEnter : Msg -> Attribute Msg
+onEnter msg =
+    let
+        isEnter code =
+            if code == 13 then
+                Json.succeed msg
+            else
+                Json.fail "not the right keycode"
+    in
+        on "keydown" (keyCode |> Json.andThen isEnter)
 
 
 
--- PROGRAM
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    section [ class "todoapp" ]
+        [ header [ class "header" ]
+            [ h1 [] [ text "todos" ]
+            , input
+                [ class "new-todo"
+                , placeholder "What needs to be done?"
+                , value model.todo.title
+                , autofocus True
+                , onEnter Add
+                , onInput UpdateTitle
+                ]
+                []
+            ]
+        , section [ class "main" ]
+            [ ul [ class "todo-list" ]
+                (List.map todoView model.todos)
+            ]
+        ]
+
+
+todoView : Todo -> Html Msg
+todoView todo =
+    li [ classList [ ( "completed", todo.completed ) ] ]
+        [ div [ class "view" ]
+            [ input [ class "toggle", type_ "checkbox", checked todo.completed ] []
+            , label [] [ text todo.title ]
+            , button [ class "destroy" ] []
+            ]
+        ]
 
 
 main : Program Never Model Msg
 main =
     Html.beginnerProgram
-        { model = model
-        , view = view
+        { model = initialModel
         , update = update
+        , view = view
         }
